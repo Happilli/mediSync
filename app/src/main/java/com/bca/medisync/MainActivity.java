677ec45f0da440158e24a84dffffa898680cba45
Happilli <133653188+Happilli@users.noreply.github.com>
2026-06-20 23:19:46 +0545

    port from ass to good[1]: - proper themes with attr for material followup - proper login layouting - state change from login to a sample placeholder for now to verify   stuffs

--- app/src/main/java/com/bca/medisync/MainActivity.java
+++ app/src/main/java/com/bca/medisync/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bca.medisync;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
 public class MainActivity extends AppCompatActivity {
+    private MaterialButton btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainStuff), (v, i)->{
+            Insets systemBars  = i.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+            return i;
+        });
+
+        btnLogin  = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(v-> {
+            Intent intent = new Intent(MainActivity.this, PlaceholderActivity.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
