fabd243896eb6410bec295d2acf27900d1f11d83
Happilli <133653188+Happilli@users.noreply.github.com>
2026-06-21 18:24:11 +0545

    feat: home screen with dashboard grid and appointment card

--- app/src/main/java/com/bca/medisync/HomeActivity.java
+++ app/src/main/java/com/bca/medisync/HomeActivity.java
@@ -7,8 +7,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bca.medisync.adapter.DashboardAdapter;
+import com.bca.medisync.data.model.Appointment;
+import com.bca.medisync.data.model.DataProvider;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+    private RecyclerView rvDashboard;
+    private BottomNavigationView  bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +29,82 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
             return insets;
         });
+        rvDashboard=findViewById(R.id.rvDashboard);
+        bottomNav=findViewById(R.id.bottomNav);
+        setupDashboard();
+        setupBottomNav();
+        setUpAppointmentCard();
+
+    }
+
+
+    private void setupDashboard(){
+        List<String> titles = Arrays.asList(
+                "Appointments",
+                "Prescriptions",
+                "Medications",
+                "My Profile",
+                "Health Records",
+                "Hospitals"
+        );
+        List<Integer> icons = Arrays.asList(
+                android.R.drawable.ic_menu_agenda,
+                android.R.drawable.ic_menu_edit,
+                android.R.drawable.ic_menu_add,
+                android.R.drawable.ic_menu_myplaces,
+                android.R.drawable.ic_menu_info_details,
+                android.R.drawable.ic_menu_compass
+        );
+
+        DashboardAdapter adapter = new DashboardAdapter(this, titles, icons, positons -> {
+            switch (positons){
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+        });
+        rvDashboard.setLayoutManager(new GridLayoutManager(this, 3));
+        rvDashboard.setAdapter(adapter);
+    }
+
+
+    private void setupBottomNav(){
+        bottomNav.setOnItemSelectedListener(item->{
+            int id = item.getItemId();
+            if (id== R.id.nav_home){
+                return true;
+            }  else if (id == R.id.nav_appointments) {
+                return true;
+            } else if (id == R.id.nav_appointments) {
+                return true;
+            } else if (id == R.id.nav_medications) {
+                return true;
+            } else if (id == R.id.nav_profile) {
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void setUpAppointmentCard(){
+        List<Appointment> appointments  = DataProvider.getAppointments();
+        if (!appointments.isEmpty()){
+            Appointment next = appointments.get(0);
+            findViewById(R.id.cardAppointment).setOnClickListener(v->{
+
+            });
+        }
     }
 }
---
+++ app/src/main/java/com/bca/medisync/adapter/DashboardAdapter.java
@@ -0,0 +1,62 @@
+package com.bca.medisync.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+import com.bca.medisync.R;
+
+import java.util.List;
+
+public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.ViewHolder> {
+    public interface OnItemClickListener{
+        void onItemClick(int position);
+    }
+
+    private final Context context;
+    private final List<String> titles;
+    private final List<Integer> icons;
+    private final OnItemClickListener listener;
+
+    public DashboardAdapter(Context context, List<String> titles, List<Integer> icons, OnItemClickListener listener) {
+        this.context = context;
+        this.titles = titles;
+        this.icons = icons;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_dashboard, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DashboardAdapter.ViewHolder holder, int position) {
+        holder.txtTitle.setText(titles.get(position));
+        holder.imgIcon.setImageResource(icons.get(position));
+        holder.itemView.setOnClickListener(v-> listener.onItemClick(position));
+    }
+    @Override
+    public int getItemCount() {
+        return titles.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgIcon;
+        TextView txtTitle;
+
+        public ViewHolder(@NotNull View itemView) {
+            super(itemView);
+            imgIcon = itemView.findViewById(R.id.imgFeature);
+            txtTitle = itemView.findViewById(R.id.txtFeature);
+        }
+    }
+}
